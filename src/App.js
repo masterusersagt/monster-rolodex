6ddabb9f6ef0897cc725d1f0dcdf3471e76aafb9
Christos Mazarakis <christos.mazarakis@avision-it.de>
2023-04-04 17:51:35 +0200

    änderungen mher mosnter

--- src/App.js
+++ src/App.js
@@ -7,15 +7,43 @@ class App extends Component {
   constructor() {
     super();
     this.state= {
+      monster1: {
+        name: 'Christos Mazarakis'
+      },
+      monster2: {
+        name: 'Master_User'
+      },
+      monster3: {
+        name: 'Jamer Kirk'
+      },
       name: {
         firstname: 'Christos',
         lastname:  'Mazarakis',
       },
-      company: 'Avision Gmbh'
+      company: 'Avision Gmbh',
+      monsters: [
+        {name: 'Christos Mazarakis'},
+        {name: 'James Kirk'},
+        {name: 'Master_User'}
+      ]
     }
   }
 
   render() {
+    return (
+    <div className='App'>
+      <h1>{this.state.monster1.name}</h1>
+      <h1>{this.state.monster2.name}</h1>
+      <h1>{this.state.monster3.name}</h1>
+      {
+        this.state.monsters.map((monster) => {
+          return <h1>{monster.name}</h1>
+        })
+      }
+    </div>);
+  }
+
+  username () {
     return (
       <div className="App">
         <header className="App-header">
@@ -30,7 +58,21 @@ class App extends Component {
               });
               console.log(this.state);
             }
-          }>Change Name</button>
+          }>Change Name example 1</button>
+          <button onClick={ () => {
+              this.setState(() => {
+                return {
+                  name: {
+                    firstname: 'Master',
+                    lastname:  'User'
+                  }
+                }
+              },
+              () => {
+                console.log(this.state);
+              }); 
+            }
+          }>Change Name example 2</button>
         </header>
       </div>
     );
