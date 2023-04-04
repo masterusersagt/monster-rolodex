cb93a324d0d6b037f5467e43932556eaa5c2b05b
Christos Mazarakis <christos.mazarakis@avision-it.de>
2023-04-04 17:09:18 +0200

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
+import { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  constructor() {
+    super();
+    this.state= {
+      name: {
+        firstname: 'Christos',
+        lastname:  'Mazarakis',
+      },
+      company: 'Avision Gmbh'
+    }
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>Hi {this.state.name.firstname} {this.state.name.lastname}, I work on {this.state.company}</p>
+          <button onClick={ () => {
+              this.setState({
+                name: {
+                  firstname: 'Master',
+                  lastname:  'User'
+                }
+              });
+              console.log(this.state);
+            }
+          }>Change Name</button>
+        </header>
+      </div>
+    );
+  }
+
 }
 
 export default App;
